f2f129099922018ea30b601908b4fe63aa963f93
unknown <yvanprogramme@gmail.com>
2021-07-26 12:53:38 +0100

    first commit

---
+++ yv.py
@@ -0,0 +1,37 @@
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+from twilio.rest import Client 
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+
+@app.route("/sms", methods=['POST'])
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+    # Fetch the message
+    msg = request.form.get('Body')
+
+    # Create reply
+    resp = MessagingResponse()
+    resp.message("You said: {} \n *Kayra Dev* ".format(msg))
+    
+    account_sid = 'AC195cf76c0d725909794c30f9b0c32961' 
+    auth_token = '70531f5d14ec79c14254cf7fdfb40bad' 
+    client = Client(account_sid, auth_token) 
+    
+    message = client.messages.create( 
+                                from_='whatsapp:+14155238886',  
+                                body=msg,      
+                                to='whatsapp:+237696527034' 
+                            ) 
+    
+    print(message.sid)
+    return str(resp)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
